a606dad41ca56d121e575ac8f551eb263115461a
Utkarsh Mishra <76392681+Utkarsh1504@users.noreply.github.com>
2021-01-31 10:28:51 +0530

    Add files via upload

---
+++ Emotion-Recognition/emotion recognition.py
@@ -0,0 +1,20 @@
+import os
+path = r'F:\CU acadamics\2020 jul-dec\ai-iot lab\exp-3'
+os.environ['PATH']+=':'+path
+from deepface import DeepFace
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+img_path = 'image4.jpg'
+img = cv2.imread(img_path)
+plt.imshow(img[:, :, ::-1])
+image = Image.open(img_path)
+image.show()
+#print(img)
+demography = DeepFace.analyze(img_path) #passing nothing as 2nd argument will find everything
+#demography = DeepFace.analyze("img4.jpg",['age', 'gender', 'emotion']) #identical to the line above
+#demography = DeepFace.analyze(["img1.jpg", "img4.jpg"]) #analyzing multiple faces same time
+print("Age: ", demography["age"])
+print("Gender: ", demography["gender"])
+print("Emotion: ", demography["dominant_emotion"])
+print("Race: ", demography["dominant_race"])
